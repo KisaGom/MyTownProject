934970b829d7505bc6c0644dbc5f2084704a8ac1
yhchoi <chanatonya@naver.com>
2022-05-19 22:49:37 +0900

    Feat: Springboot 프로젝트 생성

---
+++ mytown-springboot/src/main/java/com/ssafy/mytown/MytownSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.mytown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytownSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytownSpringbootApplication.class, args);
+	}
+
+}
---
+++ mytown-springboot/src/test/java/com/ssafy/mytown/MytownSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.mytown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytownSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
