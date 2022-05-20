d587efe983925833c657446832316d4cbc7c9790
zaq1290 <zaq1290@naver.com>
2022-05-20 10:37:58 +0900

    Feat: LifeView 관련 기능 추가 중

---
+++ mytown-vue/src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="/">NavBar</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/life">개인용</b-nav-item>
+          <b-nav-item href="#">사업용</b-nav-item>
+          <b-nav-item href="#">커뮤니티</b-nav-item>
+        </b-navbar-nav>
+
+        <b-navbar-nav class="ml-auto">
+          <!-- 로그인, 로그아웃 등 -->
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ mytown-vue/src/components/map/MapView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div id="map"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      map: null,
+    };
+  },
+  methods: {
+    initMap() {
+      const mapContainer = document.getElementById("map"); // 지도를 표시할 div
+      const mapOption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+        level: 3, // 지도의 확대 레벨
+      };
+      this.map = new kakao.maps.Map(mapContainer, mapOption);
+    },
+  },
+  mounted() {
+    if (!window.kakao || !window.kakao.maps) {
+      const script = document.createElement("script");
+      /* global kakao */
+      script.onload = () => kakao.maps.load(this.initMap);
+      script.src = `//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=703da60a86e5e83c5accc64f51991199`;
+      document.head.appendChild(script);
+    } else {
+      this.initMap();
+    }
+  },
+};
+</script>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 800px;
+}
+</style>
