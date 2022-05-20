e8d7b4c791131a6924063a307dfd5d2522903c04
yhchoi <chanatonya@naver.com>
2022-05-20 16:20:53 +0900

    Feat: LifeView 사이드바 추가

--- mytown-vue/src/views/LifeView.vue
+++ mytown-vue/src/views/LifeView.vue
@@ -1,30 +1,25 @@
 <template>
-  <div>
-    <nav-bar></nav-bar>
-    <b-row>
-      <b-col cols="1">
-        <div>
-          <b-button v-b-toggle.test>Toggle Sidebar</b-button>
-          <b-sidebar id="test" title="Sidebar" shadow>
-            <div class="px-3 py-2">
-              <p>
-                Cras mattis consectetur purus sit amet fermentum. Cras justo
-                odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
-                risus, porta ac consectetur ac, vestibulum at eros.
-              </p>
-              <b-img
-                src="https://picsum.photos/500/500/?image=54"
-                fluid
-                thumbnail
-              ></b-img>
-            </div>
-          </b-sidebar>
-        </div>
-      </b-col>
-      <b-col cols="11">
-        <map-view></map-view>
-      </b-col>
-    </b-row>
+  <div class="life-parent">
+    <div style="position: relative">
+      <nav-bar></nav-bar>
+      <div class="sidemenu side">
+        <h3>매매</h3>
+        <h3>상권</h3>
+        <h3>구성원</h3>
+      </div>
+      <div class="sidecontent side">
+        <b-form-select
+          v-model="selected1"
+          :options="options1"
+          value-field="sidocode"
+          text-field="sidoname"
+        ></b-form-select>
+        <b-form-select v-model="selected2" :options="options2"></b-form-select>
+        <b-form-select v-model="selected3" :options="options3"></b-form-select>
+        <!-- <div class="sideopener side">버튼</div> -->
+      </div>
+    </div>
+    <map-view></map-view>
   </div>
 </template>
 
@@ -34,7 +29,35 @@ import MapView from "@/components/map/MapView.vue";
 
 export default {
   components: { NavBar, MapView },
+  data() {
+    return {
+      selected1: null,
+      options1: [{ sidocode: null, sidoname: "시" }],
+      selected2: null,
+      options2: [{ value: null, text: "군" }],
+      selected3: null,
+      options3: [{ value: null, text: "동" }],
+    };
+  },
+  mounted() {},
 };
 </script>
+<style>
+.side {
+  height: 100vh;
+}
 
-<style></style>
+.sidemenu {
+  position: absolute;
+  z-index: 3;
+  background-color: azure;
+}
+
+.sidecontent {
+  position: absolute;
+  margin-left: 105px;
+  z-index: 2;
+  background-color: aquamarine;
+  transform: translateX(0%);
+}
+</style>
