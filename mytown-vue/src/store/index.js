2de94ea21a3dfc75964b8e536788ac9880a5cc92
yhchoi <chanatonya@naver.com>
2022-05-20 23:34:52 +0900

    Feat: 시군구동 입력받아서 폼에 넣기

--- mytown-vue/src/store/index.js
+++ mytown-vue/src/store/index.js
@@ -3,10 +3,14 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+import houseStore from "@/store/modules/houseStore.js";
+
 export default new Vuex.Store({
+  modules: {
+    houseStore,
+  },
   state: {},
   getters: {},
   mutations: {},
   actions: {},
-  modules: {},
 });
