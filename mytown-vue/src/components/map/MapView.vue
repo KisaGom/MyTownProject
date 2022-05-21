53dd71ee224811ebb9c34ef2410f16a6b52567e6
zaq1290 <zaq1290@naver.com>
2022-05-21 13:02:47 +0900

    Chore: vue-daum-map 사용x

--- mytown-vue/src/components/map/MapView.vue
+++ mytown-vue/src/components/map/MapView.vue
@@ -1,18 +1,10 @@
 <template>
   <div>
     <div id="map"></div>
-    <!-- <vue-daum-map
-      :appKey="appKey"
-      :center.sync="center"
-      :level.sync="level"
-      :mapTypeId="mapTypeId"
-      :libraries="libraries"
-      style="width: 100%; height: 100vh"
-    /> -->
-    <div>
+    <!-- <div>
       <div>지도중심기준 행정동 주소정보</div>
       <div id="centerAddr"></div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -37,6 +29,15 @@ export default {
 
       // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
       this.searchAddrFromCoords(this.map.getCenter(), this.displayCenterInfo);
+
+      // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+      // kakao.maps.event.addListener(this.map, "idle", function () {
+      kakao.maps.event.addListener(this.map, "idle", () => {
+        this.searchAddrFromCoords(
+          this.map.getCenter(),
+          this.displayCenterInfo()
+        );
+      });
     },
 
     searchAddrFromCoords(coords, callback) {
@@ -81,63 +82,6 @@ export default {
     }
   },
 };
-// import VueDaumMap from "vue-daum-map";
-
-// export default {
-//   components: { VueDaumMap },
-//   data() {
-//     return {
-//       appKey: "703da60a86e5e83c5accc64f51991199",
-//       center: { lat: 33.450701, lng: 126.570667 }, // 지도의 중심 좌표
-//       level: 3, // 지도의 레벨(확대, 축소 정도),
-//       mapTypeId: VueDaumMap.MapTypeId.NORMAL, // 맵 타입
-//       libraries: ["services"], // 추가로 불러올 라이브러리
-//       map: null, // 지도 객체. 지도가 로드되면 할당됨.
-//       geocoder: null,
-//       kakao: null, // 다음 API 객체. 지도가 로드되면 할당됨.
-//     };
-//   },
-//   methods: {
-//     // 지도가 로드 완료되면 load 이벤트 발생
-//     onLoad(map, daum) {
-//       this.map = map;
-//       this.kakao = daum;
-//       this.geocoder = new this.kakao.maps.services.Geocoder();
-//       // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
-//       this.searchAddrFromCoords(this.map.getCenter(), this.displayCenterInfo);
-//     },
-
-//     searchAddrFromCoords(coords, callback) {
-//       // 좌표로 행정동 주소 정보를 요청합니다
-//       this.geocoder.coord2RegionCode(
-//         coords.getLng(),
-//         coords.getLat(),
-//         callback
-//       );
-//     },
-
-//     searchDetailAddrFromCoords(coords, callback) {
-//       // 좌표로 법정동 상세 주소 정보를 요청합니다
-//       this.geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
-//     },
-
-//     // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
-//     displayCenterInfo(result, status) {
-//       console.log(status);
-//       if (status === this.kakao.maps.services.Status.OK) {
-//         var infoDiv = document.getElementById("centerAddr");
-
-//         for (var i = 0; i < result.length; i++) {
-//           // 행정동의 region_type 값은 'H' 이므로
-//           if (result[i].region_type === "H") {
-//             infoDiv.innerHTML = result[i].address_name;
-//             break;
-//           }
-//         }
-//       }
-//     },
-//   },
-// };
 </script>
 
 <style scoped>
