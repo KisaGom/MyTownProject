e4731a54aa6d21e111ed3c58708b968d0ba057c4
zaq1290 <zaq1290@naver.com>
2022-05-25 15:14:57 +0900

    Feat: 카카오 api 이용한 편의시설 상세정보 보기 추가

--- mytown-vue/src/components/life/LifeConvenience.vue
+++ mytown-vue/src/components/life/LifeConvenience.vue
@@ -25,9 +25,28 @@
       :fields="fields"
       :items="items"
       @row-clicked="(item) => $set(item, '_showDetails', !item._showDetails)"
-      ><template slot="row-details" slot-scope="row">
+    >
+      <template slot="row-details" slot-scope="row">
         <b-card>
-          {{ row.item }}
+          <b-row>
+            <b-col cols="2"><b-icon icon="geo-alt"></b-icon></b-col>
+            <b-col v-if="row.item.road_address_name" cols="10">{{
+              row.item.road_address_name
+            }}</b-col>
+            <b-col v-else cols="10">-</b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="2"><b-icon icon="telephone"></b-icon></b-col>
+            <b-col v-if="row.item.phone" cols="10">{{ row.item.phone }}</b-col>
+            <b-col v-else cols="10">-</b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="2"><b-icon icon="globe"></b-icon></b-col>
+            <b-col v-if="row.item.place_url" cols="10">{{
+              row.item.place_url
+            }}</b-col>
+            <b-col v-else cols="10">-</b-col>
+          </b-row>
         </b-card>
       </template>
     </b-table>
@@ -43,7 +62,7 @@ export default {
     return {
       fields: [
         { key: "place_name", label: "시설명" },
-        { key: "phone", label: "전화번호" },
+        { key: "category_name", label: "카테고리" },
       ],
       items: [],
       latlng: { x: 128.598409092694, y: 35.8383244008836 },
